d2fe748ef6a1f3dd28a4e645c4c5931279ff8e63
Hank Star <kate.stapley@hotmail.com>
2024-10-18 16:05:41 +1100

    starter code added

---
+++ kate-react-portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header() {
+  return (
+    <header>
+      <h1>Your Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
