7c6a7a217db5edd13e84aaafba8b074acca3c469
Hank Star <kate.stapley@hotmail.com>
2024-10-20 13:54:18 +1100

    Pausing here to work on page layout simply.

--- kate-react-portfolio/src/components/Header.js
+++ kate-react-portfolio/src/components/Header.js
@@ -4,7 +4,7 @@ import Navigation from './Navigation';
 function Header() {
   return (
     <header>
-      <h1>Your Name</h1>
+      <h1>Kate Hannah</h1>
       <Navigation />
     </header>
   );
---
+++ kate-react-portfolio/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>This is the About Me section of my portfolio.</p>
+    </section>
+  );
+}
+
+export default About;
